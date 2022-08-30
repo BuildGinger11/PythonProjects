fb39c797f06189cd9afc02b6aa6ee44e05cb92cd
BuildGinger11 <56709573+BuildGinger11@users.noreply.github.com>
2022-08-29 21:30:20 -0700

    Initial commit

---
+++ MadLibsGenerator.py
@@ -0,0 +1,22 @@
+
+noun1 = input("Choose a noun: ")
+
+pluralNoun = input("Choose a plural noun: ")
+
+adjictive = input("Choose an adjictive: ")
+
+noun2 = input("Choose a noun: ")
+
+pluralNoun2 = input("Choose a plural noun: ")
+
+adjictive2 = input("Choose an adjictive: ")
+
+name = input("Choose a name: ")
+
+place = input("Choose a place: ")
+
+print("My name is " + name + " and I love " + noun1 + "!")
+
+print("Sometimes I run around with " + noun2 + " and we have fun " + adjictive + " at " + place + "!")
+
+print ("Me and my " + pluralNoun2 + " sometimes go " + adjictive2 + ". Once in a while we invite " +noun2+ " to join us.")
