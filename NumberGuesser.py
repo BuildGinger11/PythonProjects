0a1d91cc16685fc11160ba2da9b1a32c73e9cb73
BuildGinger11 <56709573+BuildGinger11@users.noreply.github.com>
2022-08-29 22:43:24 -0700

    update

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -1,15 +1,26 @@
 import random
 
+isInt = False
 
-number = random.randrange(0, 10)
-print(number)
+def integerCheak(guessed):
+    while True:
+        #isInt = isinstance(int(guessed), int)
+        if  isInt == False:
+            guessed = input("Please type a number: ")
+            print(isinstance(guessed, int))
+        else:
+            break
+    return guessed
+
 Score = 100
 Difficulty = 0
-guess = 0
+
 continuePlaying = True
 playGame = input("hello, would you like to play a guessing game with me? (Y/N): ")
 
 while continuePlaying == True:
+    number = random.randrange(0, 10)
+    print(number)
     if playGame == "N":
         print("Ok, maybe next time")
         continuePlaying = False
@@ -22,12 +33,15 @@ while continuePlaying == True:
         else:
             Difficulty = 5
         guess = input("Great! Guess a number between 0 and 10: ")
+        #guess = integerCheak(guess)
         while int(guess) != number and Score > 0:
             if int(guess) >= number:
                 guess = input("Lower! Guess again: ")
+                #guess = integerCheak(guess)
                 Score = Score - Difficulty
             elif int(guess) <= number:
                 guess = input("Higher! Guess again: ")
+                #guess = integerCheak(guess)
                 Score = Score - Difficulty
             else:
                 guess = input("Sorry, could you try again: ")
